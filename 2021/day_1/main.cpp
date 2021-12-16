ee15b69609a6da6d2d78e2a11000055ed25e26f4
Jaret Screws <jaretscrews@gmail.com>
2021-12-16 17:09:45 -0500

    Day 1 Part 2

--- 2021/day_1/main.cpp
+++ 2021/day_1/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <limits>
+#include <vector>
 
-int main(){
+int part1() {
     std::ifstream infile("input.txt");
     int curr = std::numeric_limits<int>::max();
     int prev = 0;
@@ -16,5 +17,36 @@ int main(){
             res++;
     }
 
-    std::cout << res << std::endl;
+    return res;
+}
+
+int part2() {
+    std::ifstream infile("input.txt");
+    std::vector<int> sums;
+    int x, y, z;
+    infile >> x >> y >> z;
+    int res = 0;
+    while (infile)  
+    {
+        int sum = x + y + z;
+        sums.push_back(sum);
+        sum -= x;
+        x = y;
+        y = z;
+        infile >> z;
+    }
+
+    for(unsigned int i = 0; i < sums.size(); i++)
+    {
+        if (sums[i] < sums[i + 1])
+            res++;
+    }
+    
+    return res;
+}
+
+
+int main() {
+    std::cout << part1() << std::endl;
+    std::cout << part2() << std::endl;
 }
