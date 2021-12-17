d3be947e7dc46a2a8cea6e1cfd73119f13e68b09
Jaret Screws <jaretscrews@gmail.com>
2021-12-16 22:21:32 -0500

    Day 3 done int vs float math messed me up for a while

--- 2021/day_2/main.cpp
+++ 2021/day_2/main.cpp
@@ -1,3 +1,62 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#define FILE_PATH "day_2.txt"
+
+int part1() {
+    int distance = 0;
+    int depth = 0;
+    std::fstream file(FILE_PATH);
+    while(file) {
+        std::string line;
+        std::getline(file, line);
+        std::stringstream in(line);
+        std::string command;
+        int x;
+        in >> command >> x;
+        if(command == "forward") {
+            distance += x;
+        }
+        else if(command == "down") {
+            depth += x;
+        }
+        else if(command == "up") {
+            depth -= x;
+        }
+    }
+    std::cout << distance << " " << depth << std::endl;
+    return distance * depth;
+}
+
+int part2() {
+    int distance = 0;
+    int depth = 0;
+    int aim = 0;
+    std::fstream file(FILE_PATH);
+    while(file) {
+        std::string line;
+        std::getline(file, line);
+        std::stringstream in(line);
+        std::string command;
+        int x;
+        in >> command >> x;
+        if(command == "forward") {
+            distance += x;
+            depth += (aim * x);
+        }
+        else if(command == "down") {
+            aim += x;
+        }
+        else if(command == "up") {
+            aim -= x;
+        }
+    }
+    std::cout << distance << " " << depth << std::endl;
+    return distance * depth;
+}
 
 int main() {
+    std::cout << part1() << std::endl;
+    std::cout << part2() << std::endl;
 }
